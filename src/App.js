3f24494c464148f7c26c112d2bed0a9fd2e9b1f0
Tanusree Sen <30462968+Tanushree77@users.noreply.github.com>
2021-02-20 20:40:41 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react";
+import "./App.css";
+import Light from './components/light/light';
+import {store} from './redux/store';
+import {Provider} from "react-redux";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const App =() => {
+  return (
+    <Provider store={store}>
+      <React.Fragment>
+          <Light/>
 
-  handleClick = api => e => {
-    e.preventDefault()
+      </React.Fragment>
+    </Provider>
+  );
+};
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
